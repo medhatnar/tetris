fb21c0b22497aa2a629b85277da613aee712d738
narmander <narminshahin@gmail.com>
2020-03-08 19:01:52 -0400

    fixup^

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-const titleScreen = new TitleScreen();
+// const titleScreen = new TitleScreen();
 
-let music;
-let image1;
+// let music;
+// let image1;
 
-function preload() {
- music = loadSound("./file/path/to/sound.mp3");
- image1 = loadImage("./file/path/to/sound.jpg");
-}
+// function preload() {
+//  music = loadSound("./file/path/to/sound.mp3");
+//  image1 = loadImage("./file/path/to/sound.jpg");
+// }
 
-function setup() {
-  createCanvas(900, 900);
-}
+// function setup() {
+//   createCanvas(900, 900);
+// }
 
-function draw() {
-  background(0);
-  titleScreen.display();
-}
+// function draw() {
+//   background(0);
+//   titleScreen.display();
+// }
