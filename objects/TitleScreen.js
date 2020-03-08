c561eea189a474921e77055e8c49964bd7f7c153
narmander <narminshahin@gmail.com>
2020-03-08 19:01:52 -0400

    fixup^

--- objects/TitleScreen.js
+++ objects/TitleScreen.js
@@ -4,35 +4,52 @@ var height = 900;
 // set to 0 if not being used
 
 function TitleScreen() {
-  this.display = function() {
-    push();
-    angleMode(DEGREES);
-    textFont("VT323");
-    textSize(350);
-    strokeWeight(10);
-    stroke(255, 255, 255);
-    fill(91, 207, 250);
-    textAlign(CENTER, CENTER);
-    text("Q", 200, 250);
-    text("T", 550, 250);
-    textSize(50);
-    text("u e e r", 360, 300);
-    text("e t r i s", 700, 300);
-    pop();
-  }
+	this.display = function() {
+		push();
+		angleMode(DEGREES);
+		textFont('VT323');
+		textSize(350);
+		strokeWeight(10);
+		stroke(255, 255, 255);
+		fill(91, 207, 250);
+		textAlign(CENTER, CENTER);
+		text('Q', 200, 250);
+		text('T', 550, 250);
+		textSize(50);
+		text('u e e r', 360, 300);
+    text('e t r i s', 700, 300);
+    image(arrows.up, 380, 440)
+    image(arrows.left, 180, 535)
+    image(arrows.right, 750, 535)
+    image(arrows.down, 360, 640)
+    textSize(40);
+    stroke(0)
+    fill(255, 33, 140);
+    text('rotate', 515, 455);
+    fill(0, 0, 254);
+    text('go left', 300, 545);
+    fill(188, 39, 252);
+    text('go right', 660, 545);
+    fill(255, 33, 140);
+    text('death drop', 520, 650);
+    text('press <Enter> to start', 500, 750);
+		pop();
+	};
 }
 
 const titleScreen = new TitleScreen();
 
-let soundEffect1;
-let image1;
+let arrows = {};
 
 function preload() {
-  //soundEffect1 = loadSound("./file/path/to/sound.mp3");
+	arrows.up = loadImage('../assets/images/arrows/arrowUp.png');
+	arrows.left = loadImage('../assets/images/arrows/arrowLeft.png');
+	arrows.right = loadImage('../assets/images/arrows/arrowRight.png');
+	arrows.down = loadImage('../assets/images/arrows/arrowDown.png');
 }
 
 function setup() {
-  createCanvas(900, 900);
+	createCanvas(900, 900);
 }
 
 function draw() {
