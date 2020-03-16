cba0d11dcdbd386842255275c52e0070216737af
narmander <narminshahin@gmail.com>
2020-03-16 13:40:26 -0400

    working on score screen

--- objects/ScoreScreen.js
+++ objects/ScoreScreen.js
@@ -1,3 +1,19 @@
+function ScoreScreen() {
+    this.display = function() {}
+}
+
+const titleScreen = new TitleScreen();
 
 
+function preload() {
+}
+
+function setup() {
+	createCanvas(900, 900);
+}
+
+function draw() {
+  background(0);
+	ScoreScreen.display();
+}
 
