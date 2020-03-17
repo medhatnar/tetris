bd7e640b07db97a8b4f980b77ecafe8c9ac44d01
narmander <narminshahin@gmail.com>
2020-03-17 08:27:31 -0400

    adding input field

---
+++ server/InitFirebase.js
@@ -0,0 +1,15 @@
+var config = config();
+
+var firebaseConfig = {
+				apiKey: config.firebase_key,
+				authDomain: `${config.projectId}.firebaseapp.com`,
+				databaseURL: `https://${config.projectId}.firebaseio.com`,
+				projectId: config.projectId,
+				storageBucket: `${config.projectId}.appspot.com`,
+				messagingSenderId: config.msgSendId,
+				appId: config.appId,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database();
