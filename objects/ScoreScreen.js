d70b3664e79242c5ea155776b7bcec8b3a17fa75
narmander <narminshahin@gmail.com>
2020-03-22 11:15:36 -0400

    rank to high score transition working

--- objects/ScoreScreen.js
+++ objects/ScoreScreen.js
@@ -1,53 +1,58 @@
 function ScoreScreen() {
+	var scores;
+	var yourRankScores;
 	var highScores;
+	var startScores;
 	var headerText = 'YOUR RANKING';
 	var yCor = -100;
 	var xCor = gameArea.size + 10;
-	var headerInterval;
-	var rowInterval;
-	var nameXPos;
-	var scoreXPos;
-	var dateXPos;
+	var headerInterval;;
 	var dataCalled = false;
 
-	this.display = function() {
-		if(dataCalled === false) { readScores(); dataCalled = true;};
+	this.display = async function() {
+		if (dataCalled === false) {
+			scores = await readScores();
+			console.log("Getting high scores", scores);
+			dataCalled = true;
+		};
 		textAlign(CENTER);
 		stroke(yellow);
 		fill(0, 0, 0);
 		textSize(150);
 		text(headerText, 450, yCor);
+
 		if (!headerInterval) {
 			headerInterval = setInterval(() => {
 				yCor += 10;
 				if (yCor === 100) {
 					clearInterval(headerInterval);
-				}
+				};
 			}, 50);
-		}
+		};
 
 		stroke(darkBlue);
 		fill(yellow);
 		textSize(100);
 		text('NAME', 150, yCor + 100);
 		text('SCORE', 450, yCor + 100);
-		text('DATE', 750, yCor + 100);
+		text('RANK', 750, yCor + 100);
 
-		if (yCor === 100 && highScores) {
-			console.log(highScores);
-			var yCorOffset = 200;
+		if (yCor === 100) {
 			textSize(50);
-			text('NAME', xCor, yCor + yCorOffset);
-			text('9001', xCor + 260, yCor + yCorOffset);
-			text('03/19/20', xCor + 615, yCor + yCorOffset);
-			if (!rowInterval) {
-				rowInterval = setInterval(() => {
-					if (xCor === 150) {
-						yCorOffset += 100;
-						clearInterval(rowInterval);
-					}
-					xCor -= 10;
-				}, 20);
+			var yCorOffset = 200;
+			if (frameCount % 2 === 0 && xCor !== 150) xCor -= 10;
+			
+			if(xCor === 150 && frameCount === 600) {
+				xCor = gameArea.size + 10;
+				yCorOffset = 200;
+				headerText = "QT HIGH SCORES";
+			}
+
+			for (let i = 0; i < 12; i++) {
+				text('NAME', xCor, yCor + yCorOffset);
+				text('9001', xCor + 260, yCor + yCorOffset);
+				text('5th', xCor + 615, yCor + yCorOffset);
+				yCorOffset += 50;
 			}
 
 			fill(0, 0, 0);
@@ -61,14 +66,14 @@ function ScoreScreen() {
 		}
 	};
 
-	async function readScores() {
-		return await db
-			.ref('highScores')
+	function readScores() {
+		return db
+			.ref('scores')
 			.once('value')
-			.then(function(snapshot) {
+			.then(snapshot => {
 				var response = snapshot.val();
-				highScores = response;
-				return highScores;
+				console.log('response', response);
+				return response;
 			});
 	}
 }
